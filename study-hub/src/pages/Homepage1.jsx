e298e0792969928e547b60ae9e3db7043753ff8c
elidaarnaut <elida.arnaut@gmail.com>
2024-05-16 00:11:05 +0200

    Finished Homepage1

--- study-hub/src/pages/Homepage1.jsx
+++ study-hub/src/pages/Homepage1.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import TopMenu1 from "../components/TopMenu1";
+import hpGirl from "../assets/hpGirl.svg";
 
 function Homepage1() {
     return(
@@ -11,12 +12,12 @@ function Homepage1() {
                     <p>Welcome to our e-learning platform, where traditional education meets modern technology. 
 We offer personalized profiles, AI-driven grading, and interactive labs, making learning efficient and engaging. </p>
                     <br />
-                    <p>Join us as we revolutionize education together.</p>
+                    <p className="mainPInvite">Join us as we revolutionize education together.</p>
                     <button className="button">Sign up now</button>
 
                 </div>
                 <div className="mainPhoto">
-
+                    <img src={hpGirl} alt="Girl Studying Illustration" />
                 </div>
 
             </div>
