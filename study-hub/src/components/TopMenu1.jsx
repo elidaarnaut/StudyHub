320009ab85a06b28b42e47672a1a883df2d480fa
elidaarnaut <elida.arnaut@gmail.com>
2024-05-15 18:10:17 +0200

    Top Menu Finished

--- study-hub/src/components/TopMenu1.jsx
+++ study-hub/src/components/TopMenu1.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Search1 from "./Search1";
+import Container from 'react-bootstrap/Container';
+
 import "../App.css"
 
 function TopMenu1(){
     return(
         <div className="topMenu1div">
             <Search1></Search1>
+            <div>
+                <button className="button">Log in</button>
+                <button className="button">Sign up</button>
+            </div>
+            
         </div>
     );
 }
