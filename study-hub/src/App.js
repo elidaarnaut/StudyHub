c5d55ea7552a1e6204a4fc63d63ff86b1993deeb
Dalila Salcin <dalilasalcin@hotmail.com>
2024-05-16 15:14:17 +0200

    Updated SelectProfile.js and added that screens added have dynamic between

--- study-hub/src/App.js
+++ study-hub/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-//import SignUp1 from './pages/SignUp1';
+import SelectProfile from './pages/SelectProfile';
+import SignUp1 from './pages/SignUp1'; // signup for students
 import SignUp2 from './pages/SignUp2'; // signup for instructors step 1
 import VerificationsUpload from './pages/VerificationsUpload'; // signup for instructors step 2
 
@@ -9,9 +10,11 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
+          <Route path="/select-profile" element={<SelectProfile />} />
+          <Route path="/signup-student" element={<SignUp1 />} />
           <Route path="/signup-instructor" element={<SignUp2 />} />
           <Route path="/verification" element={<VerificationsUpload />} />
-          <Route path="/" element={<SignUp2 />} /> {/* Default route */}
+          <Route path="/" element={<SelectProfile />} /> {/* Default route */}
         </Routes>
       </div>
     </Router>
--- study-hub/src/pages/SelectProfile.js
+++ study-hub/src/pages/SelectProfile.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Card, Container, Row, Col } from 'react-bootstrap';
-import StudentImage from '../assets/student.svg';
-import InstructorImage from '../assets/instructor.svg';
+import StudentImage from '../assets/student.jpg';
+import InstructorImage from '../assets/instructor.jpg';
 import WebFont from 'webfontloader';
 
 WebFont.load({
@@ -12,6 +13,8 @@ WebFont.load({
 });
 
 function SelectProfile() {
+    const navigate = useNavigate();
+
     const containerStyle = {
         marginTop: '5rem',
         backgroundColor: '#FFFFFF',
@@ -23,27 +26,50 @@ function SelectProfile() {
         fontWeight: '700'  // Bold font weight
     };
 
-   
+    const cardStyle = {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%',
+        border: 'none',  // Remove border
+        padding: '1rem',
+        boxShadow: 'none',  // Remove shadow
+        cursor: 'pointer'  // Change cursor to pointer to indicate clickability
+    };
 
     const imageStyle = {
         width: '100%',  
-        height: '100%'  
+        height: 'auto',
+        borderRadius: '20px'  // Add border radius to the image
+    };
+
+    const colStyle = {
+        margin: '0 1rem'  // Add margin to the columns to create space between cards
+    };
+
+    const handleInstructorClick = () => {
+        navigate('/signup-instructor');
+    };
+
+    const handleStudentClick = () => {
+        navigate('/signup-student');
     };
 
     return (
         <Container style={containerStyle}>
             <h2 className="text-center mb-4" style={titleStyle}>Please choose account type!</h2>
             <Row className="d-flex justify-content-center">
-                <Col md={6} lg={4} className="mb-4 mx-2">  
-                    <Card className="text-center">
+                <Col md={6} lg={4} className="mb-4 d-flex align-items-stretch" style={colStyle} onClick={handleStudentClick}>
+                    <Card className="text-center" style={cardStyle}>
                         <Card.Img variant="top" src={StudentImage} style={imageStyle} />
-                       
+                        <Card.Body></Card.Body>
                     </Card>
                 </Col>
-                <Col md={6} lg={4} className="mb-4 mx-2"> 
-                    <Card className="text-center">
+                <Col md={6} lg={4} className="mb-4 d-flex align-items-stretch" style={colStyle} onClick={handleInstructorClick}>
+                    <Card className="text-center" style={cardStyle}>
                         <Card.Img variant="top" src={InstructorImage} style={imageStyle} />
-                        
+                        <Card.Body></Card.Body>
                     </Card>
                 </Col>
             </Row>
