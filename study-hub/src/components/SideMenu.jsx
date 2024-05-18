80129163637315028989ab8ed4c68fc4aa79fa42
elidaarnaut <elida.arnaut@gmail.com>
2024-05-18 18:23:50 +0200

    Side Menu version 1 complete

--- study-hub/src/components/SideMenu.jsx
+++ study-hub/src/components/SideMenu.jsx
@@ -2,8 +2,18 @@ import React from "react";
 
 function SideMenu(){
     return(
-        <div>
-
+        <div className="sideMenuDiv">
+            <h2 className="sideMenuTitle">EXPLORE</h2>
+            <p>Instructors</p>
+            <p>Courses</p>
+            <p>AI Chat</p>
+            <p>Virtal Lab</p>
+            <p>Subjects</p>
+            <hr />
+            <h2 className="sideMenuTitle">MY ACCOUNT</h2>
+            <p>Profile</p>
+            <p>Settings</p>
+            <p>Log out</p>
         </div>
     );
 }
--- study-hub/src/pages/DashboardStudent.jsx
+++ study-hub/src/pages/DashboardStudent.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import TopMenu2 from "../components/TopMenu2";
+import SideMenu from "../components/SideMenu";
 
 function DashboardStudent(){
     return(
-        <div>
+        <div >
             <TopMenu2></TopMenu2>
+            <div className="sDashboardMain">
+                <SideMenu></SideMenu>
+            </div>
         </div>
     );
 }
