95b1b034897872aa0f453902a1bd8e243845bd70
elidaarnaut <elida.arnaut@gmail.com>
2024-05-19 18:36:40 +0200

    Progres on Students Dashboard

---
+++ study-hub/src/components/MySubjectDash.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MySubjectsDash(){
+    return (
+        <div className="subjectItemDash">
+            <p>Subject 1</p>
+         </div>
+    );
+}
+
+export default MySubjectsDash;
--- study-hub/src/pages/DashboardStudent.jsx
+++ study-hub/src/pages/DashboardStudent.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import TopMenu2 from "../components/TopMenu2";
 import SideMenu from "../components/SideMenu";
+import Badges from "../components/Badges";
+import MySubjectsDash from "../components/MySubjectDash";
 
 function DashboardStudent(){
     return(
@@ -8,6 +10,23 @@ function DashboardStudent(){
             <TopMenu2></TopMenu2>
             <div className="sDashboardMain">
                 <SideMenu></SideMenu>
+                <div className="next2SideMenuDiv">
+                    <div className="badgesSectionDash">
+                        <h1>My Badges</h1>
+                        <hr />
+                        <Badges></Badges>
+                    </div>
+                    <div className="mySubjectsDash">
+                        <h1>My Subjects</h1>
+                        <hr />
+                        <div className="subjectsListedDash">
+                            <MySubjectsDash></MySubjectsDash>
+                            <MySubjectsDash></MySubjectsDash>
+                            <MySubjectsDash></MySubjectsDash>
+                        </div>
+                        
+                    </div>
+                </div>
             </div>
         </div>
     );
