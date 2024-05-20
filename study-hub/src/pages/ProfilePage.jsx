1ffa883fc4ff6bedb36e547fa5ee56b29b3fe7fa
elidaarnaut <elida.arnaut@gmail.com>
2024-05-20 14:44:22 +0200

    Progres on profile page

--- study-hub/src/pages/ProfilePage.jsx
+++ study-hub/src/pages/ProfilePage.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import TopMenu2 from "../components/TopMenu2";
 import SideMenu from "../components/SideMenu";
+import banner from "../assets/banner.svg";
+import profilePic from "../assets/profilePic.svg";
 
 function ProfilePage(){
     return(
@@ -8,8 +10,8 @@ function ProfilePage(){
             <TopMenu2></TopMenu2>
             <SideMenu></SideMenu>
             <div className="next2SideMenuDiv">
-                <h1>hi</h1>
-
+                <img src={banner} alt="Banner" className="banner" />
+                <img src={profilePic} alt="Profile Picture" />
             </div>
 
         </div>
