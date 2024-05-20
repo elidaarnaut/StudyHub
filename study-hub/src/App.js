bfdd549a026c8736350c127cfc5beaa8cb76b62b
Dalila Salcin <dalilasalcin@hotmail.com>
2024-05-20 23:33:47 +0200

    started gradepage.js

--- study-hub/src/App.js
+++ study-hub/src/App.js
@@ -4,16 +4,21 @@ import SelectProfile from './pages/SelectProfile';
 import SignUp1 from './pages/SignUp1'; // signup for students
 import SignUp2 from './pages/SignUp2'; // signup for instructors step 1
 import VerificationsUpload from './pages/VerificationsUpload'; // signup for instructors step 2
+import GradePage from './pages/GradePage';
+import DashboardInstructor from './pages/DashboardInstructor'; // Import the DashboardInstructor component
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
+          
+        <Route path="/grade-page" element={<GradePage />} />
           <Route path="/select-profile" element={<SelectProfile />} />
           <Route path="/signup-student" element={<SignUp1 />} />
           <Route path="/signup-instructor" element={<SignUp2 />} />
           <Route path="/verification" element={<VerificationsUpload />} />
+          <Route path="/dashboard-instructor" element={<DashboardInstructor />} /> {/* Add the route for DashboardInstructor */}
           <Route path="/" element={<SelectProfile />} /> {/* Default route */}
         </Routes>
       </div>
---
+++ study-hub/src/pages/GradePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import TopMenu2 from "../components/TopMenu2";
+import SideMenu3 from "../components/SideMenu3";
+
+function GradePage(){
+    return(
+        <div>
+            <TopMenu2></TopMenu2>
+            
+        </div>
+    );
+}
+
+export default GradePage;
