d4f0c0f0bd88a7ce1c533a16bc2fd4c3d14d58ff
elidaarnaut <elida.arnaut@gmail.com>
2024-05-21 20:25:36 +0200

    Profile page version 1 complete

--- study-hub/src/pages/ProfilePage.jsx
+++ study-hub/src/pages/ProfilePage.jsx
@@ -3,6 +3,7 @@ import TopMenu2 from "../components/TopMenu2";
 import SideMenu from "../components/SideMenu";
 import banner from "../assets/banner.svg";
 import profilePic from "../assets/profilePic.svg";
+import newcomerBadge from "../assets/newcomerBadge.svg";
 
 function ProfilePage(){
     return(
@@ -11,7 +12,38 @@ function ProfilePage(){
             <SideMenu></SideMenu>
             <div className="next2SideMenuDiv">
                 <img src={banner} alt="Banner" className="banner" />
-                <img src={profilePic} alt="Profile Picture" />
+                <div className="ProfilePicDiv">                
+                    <img src={profilePic} alt="Profile Picture" className="profilePic"/>
+                </div>
+                <div className="profileContents">
+                    <div className="profileSides1">
+                        <p className="profileName">Jane Doe</p>
+                        <p className="profileEmail">jane.doe@gmail.com</p>
+                        <hr />
+                        <p className="profileBio">Bio</p>
+                        <div className="profileBioText sectionDiv">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere iusto nemo incidunt at saepe autem quos minima eaque ducimus possimus similique facilis laudantium officiis dolores, cupiditate, distinctio deserunt nihil! Autem.
+                        </div>
+                    </div>
+                    <div className="profileSides2 sectionDiv">
+                        <p>My Badges</p>
+                        <br />
+                        
+                        <div className="badgesContainer">
+                           <img src={newcomerBadge} alt="Badge Image" className="profileBadgeImg" />
+                            <img src={newcomerBadge} alt="Badge Image" className="profileBadgeImg" />
+                            <img src={newcomerBadge} alt="Badge Image" className="profileBadgeImg" />
+                            <img src={newcomerBadge} alt="Badge Image" className="profileBadgeImg" />
+                            <img src={newcomerBadge} alt="Badge Image" className="profileBadgeImg" />
+                        </div>
+                        <div className="overlayProfile">
+                                <p>See More</p>
+                        </div>
+                        
+                    </div>
+
+                </div>
+                
             </div>
 
         </div>
