074b469bbb6bc81151b3047b5b45eca197390073
elidaarnaut <elida.arnaut@gmail.com>
2024-06-11 08:18:28 +0200

    Layout for Instructor profile page

--- study-hub/src/pages/ProfilePageInstructor.jsx
+++ study-hub/src/pages/ProfilePageInstructor.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import TopMenu4 from "../components/TopMenu4";
 import banner from "../assets/banner.svg";
 import profilePic from "../assets/profilePic.svg";
+import CourseCard from "../components/CourseCard";
 import newcomerBadge from "../assets/newcomerBadge.svg";
 
 function ProfilePageInstructor(){
@@ -15,32 +16,31 @@ function ProfilePageInstructor(){
                     <img src={profilePic} alt="Profile Picture" className="profilePic"/>
                 </div>
                 <div className="profileContents">
-                    <div className="profileSides1">
+                    <div className="profileSides">
                         <p className="profileName">Jane Doe</p>
                         <p className="profileEmail">jane.doe@gmail.com</p>
-                        <hr />
-                        <p className="profileBio">Bio</p>
-                        <div className="profileBioText sectionDiv">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere iusto nemo incidunt at saepe autem quos minima eaque ducimus possimus similique facilis laudantium officiis dolores, cupiditate, distinctio deserunt nihil! Autem.
-                        </div>
-                    </div>
-                    <div className="profileSides2 sectionDiv">
-                        <p>My Badges</p>
-                        <br />
                         
-                        <div className="badgesContainer">
-                           <img src={newcomerBadge} alt="Badge Image" className="profileBadgeImg" />
-                            <img src={newcomerBadge} alt="Badge Image" className="profileBadgeImg" />
-                            <img src={newcomerBadge} alt="Badge Image" className="profileBadgeImg" />
-                            <img src={newcomerBadge} alt="Badge Image" className="profileBadgeImg" />
-                            <img src={newcomerBadge} alt="Badge Image" className="profileBadgeImg" />
-                            <Link to="/badgesPage" className="noLink seeMoreLink"><button class="card-button1">See More</button></Link>
+                    </div>
+                    <div className="profileSides3 sectionDiv">
+                        <p className="profileBio">Bio</p>
+                        <div className="profileBioText ">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere iusto nemo incidunt at saepe autem quos minima eaque ducimus possimus similique facilis laudantium officiis dolores, cupiditate, distinctio deserunt nihil! Autem.
                         </div>
                         
                         
                     </div>
 
                 </div>
+                <div className="coursesSection1">
+                        <h1 className="instructorsTitle">My Courses</h1>
+                        <hr className="hrDash" />
+                        <div className="coursesCardLayout">
+                            <CourseCard></CourseCard>
+                            <CourseCard></CourseCard>
+                            <CourseCard></CourseCard>
+                        </div>
+
+                    </div>
                 
             </div>
 
