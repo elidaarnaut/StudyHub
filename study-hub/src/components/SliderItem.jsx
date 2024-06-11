15c0681ae1d75993ff66655641ceb6208360c34d
elidaarnaut <elida.arnaut@gmail.com>
2024-06-11 09:10:49 +0200

    Change in Slider item

--- study-hub/src/components/SliderItem.jsx
+++ study-hub/src/components/SliderItem.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Slider.css';  
+import subjectCoverMath from "../assets/mathSubject.jpg";
 
-const SliderItem = ({ content, active }) => {
-    const itemClass = active ? "slider-item active" : "slider-item";
+const SliderItem = ({ id, content }) => {
     return (
-        <div className={itemClass}>
+        <div className="slider-item" id={id}>
             <div className="slider-item-content">
+                <div className='subjectPic'>
+                    <img src={subjectCoverMath} alt={content} />
+                </div>
                 <p>{content}</p>
             </div>
         </div>
