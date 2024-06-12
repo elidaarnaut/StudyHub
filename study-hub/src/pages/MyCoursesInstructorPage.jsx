aeace7fdf52dcb8a28b1e764efd158f63cb07b6b
elidaarnaut <elida.arnaut@gmail.com>
2024-06-12 07:51:13 +0200

    Instructor Profile

--- study-hub/src/pages/MyCoursesInstructorPage.jsx
+++ study-hub/src/pages/MyCoursesInstructorPage.jsx
@@ -1,6 +1,34 @@
 import React from "react";
 import TopMenu4 from "../components/TopMenu4";
 import CourseCard from "../components/CourseCard";
+import person5 from "../assets/person5.jpg"; // Update the import path as necessary
+
+const courses = [
+    {
+        title: "Advanced Calculus for High School",
+        description: "Explore differential and integral calculus with applications in real-world problems, designed for advanced high school students.",
+        instructorName: "Emily Cartwright",
+        instructorEmail: "emily.cartwright@edututors.com",
+        instructorImg: person5,
+        price: 40,
+    },
+    {
+        title: "AP Mathematics Preparation",
+        description: "Get ready for the AP exams with intensive problem-solving sessions and comprehensive material coverage.",
+        instructorName: "Emily Cartwright",
+        instructorEmail: "emily.cartwright@edututors.com",
+        instructorImg: person5,
+        price: 40
+    },
+    {
+        title: "Interactive Mathematics Workshops",
+        description: "Participate in engaging workshops that enhance your understanding of mathematics through practical applications and group activities.",
+        instructorName: "Emily Cartwright",
+        instructorEmail: "emily.cartwright@edututors.com",
+        instructorImg: person5,
+        price: 40
+    }
+];
 
 function MyCoursesInstructorPage(){
     return(
@@ -9,18 +37,15 @@ function MyCoursesInstructorPage(){
             <div className="sDashboardMain">
                 <div className="next2SideMenuDiv">
                     <div className="coursesSection">
-                        <h1 className="instructorsTitle">Courses Page</h1>
+                        <h1 className="instructorsTitle">My Courses</h1>
                         <hr className="hrDash" />
                         <div className="coursesCardLayout">
-                            <CourseCard></CourseCard>
-                            <CourseCard></CourseCard>
-                            <CourseCard></CourseCard>
+                            {courses.map((course, index) => (
+                                <CourseCard key={index} {...course} />
+                            ))}
                         </div>
-
                     </div>
-
                 </div>
-
             </div>
         </div>
     );
